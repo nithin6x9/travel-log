4b05f32b963f97eb59ae37ca52f8b05fc86f498c
nithin-nk <nithinkrishnalal303@gmail.com>
2024-01-10 01:41:20 +0530

    backend express

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+//Travel - log
+
+const express = require("express");
+const app = express();
+
+
+app.get("/travelLog",(req,res)=>{
+
+})
+
+app.post("/travelLog",(req,res)=>{
+
+})
+
+app.put("/travelLog",(req,res)=>{
+
+})
+
+app.listen(3000);
